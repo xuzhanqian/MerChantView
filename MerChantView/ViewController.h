05949b880dea328240db665e40bc7e779fea54fd
apple <apple@appledeMacBook-Pro.local>
2015-01-07 13:24:36 +0800

    Initial Commit

---
+++ MerChantView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MerChantView
+//
+//  Created by apple on 15-1-7.
+//  Copyright (c) 2015年 xuzhanqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MerChantView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MerChantView
+//
+//  Created by apple on 15-1-7.
+//  Copyright (c) 2015年 xuzhanqian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
